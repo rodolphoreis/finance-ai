25f06f288d9427d5f05283ce9317c1c683178c54
Rodolpho Reis <rodolphoreis@live.com>
2024-11-07 15:27:49 +0000

    chore: add shadcn setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="pt-PT">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="grid h-screen w-full grid-cols-2">
+      <div className="h-full">
+        <Image
+          src="/img-bitcoin.jpeg"
+          alt="bitcoin"
+          width={500}
+          height={500}
+          className="h-screen w-full object-cover"
+        />
+      </div>
+      <div className="h-full">
+        <h1>New</h1>
+      </div>
     </main>
   );
 }
